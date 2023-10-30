0dff7915598f7b97ba3af570b30bd933d6faefca
Jessie11019 <146058017+Jessie11019@users.noreply.github.com>
2023-10-30 22:20:41 +0800

    hw4-2.c

--- hw4-2.c
+++ hw4-2.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdbool.h>
-#include <assert.h>
 
 int main(void) {
   int atm[6][3] = {{123, 456, 9000}, {456, 789, 5000}, {789, 888, 6000}, {336, 558, 10000}, {775, 666, 12000}, {566, 221, 7000}};
@@ -8,7 +7,6 @@ int main(void) {
 
   int n;
   scanf("%d",&n);
-  assert(n>=1&&n<=6);
 
   for (int a = 0; a < n; a++)
   {
